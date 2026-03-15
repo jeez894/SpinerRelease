83a36192d1516a0e781d8f2b82b530ea0b250d39
jeez894 <pow666@hotmail.fr>
2026-03-15 19:20:11 +0100

    Clean repo structure for Spiner 2.0

---
+++ Git/StubSpinerVisual/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Spiner
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "SpinerVisual";
+        public const string PLUGIN_NAME = "SpinerVisual";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
